46e9a7e0e152a6c8556c5753469a7c6556a02d94
Yoli Meydan <meydany@bxscience.edu>
2015-05-30 11:56:03 -0400

    Enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,36 +7,24 @@ public enum SpawnSide {
 }
 
 public class Enemy : MonoBehaviour {
-
-	public float randomXPos, randomYPos;
-
+	
 	private float randomXSpeed;
 	private float randomYSpeed;
 	
 	private Vector3 enemyPos;
 
-	private float randomSide;
-	public SpawnSide side;
 	
 	// Use this for initialization
 	void Start () {
 		randomXSpeed = Random.Range (1f, 3f);
 		randomYSpeed = Random.Range (-2f, 2f);
 
-		randomSide = Random.Range (0f, 2f);
 
-		if (randomSide < 1) {
-			side = SpawnSide.LEFT;
-			transform.position = new Vector3(-(Camera.main.orthographicSize * Camera.main.aspect + (GetComponent<Renderer>().bounds.size.x /2)), Random.Range(-(Camera.main.orthographicSize), Camera.main.orthographicSize), 0);
-		} else {
-			side = SpawnSide.RIGHT;
-			transform.position = new Vector3((Camera.main.orthographicSize * Camera.main.aspect + (GetComponent<Renderer>().bounds.size.x /2)), Random.Range(-(Camera.main.orthographicSize), Camera.main.orthographicSize), 0);
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//moveEnemy ();
+		moveEnemy ();
 	}
 
 	void moveEnemy() {
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -8,13 +8,21 @@ public class EnemyManager : MonoBehaviour {
 	private float xPos, yPos;
 	// Use this for initialization
 	void Start () {
-		xPos = 0f;
-		yPos = 0f;
-		Instantiate (enemyPrefab, new Vector3 (xPos, yPos, 0f), Quaternion.identity);
+
+		if (enemyPrefab.GetComponent<Enemy> ().side == SpawnSide.LEFT) {
+			xPos = Random.Range (-Camera.main.orthographicSize * Camera.main.aspect * 2f,-Camera.main.orthographicSize * Camera.main.aspect);
+			yPos = Random.Range (-Camera.main.orthographicSize,Camera.main.orthographicSize);
+		} else if(enemyPrefab.GetComponent<Enemy> ().side == SpawnSide.RIGHT){
+			print ("im here");
+			xPos = Random.Range (Camera.main.orthographicSize * Camera.main.aspect,Camera.main.orthographicSize * Camera.main.aspect * 2f);
+			yPos = Random.Range (-Camera.main.orthographicSize, Camera.main.orthographicSize);
+		}
+
+		Instantiate (enemyPrefab, new Vector3 (xPos, yPos, 0), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Random.Range
 	}
 }
