37ef46148add10a70f98adb3cc6a109056a2b8c0
Yoli Meydan <meydany@bxscience.edu>
2015-05-30 15:36:56 -0400

    Started Photon Networking

---
+++ Assets/Scripts/Multiplayer Scrips/PhotonNetworkingManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonNetworkingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.sendRate = 20;
+		PhotonNetwork.sendRateOnSerialize = 10;
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onPhotoSerializedView(PhotonStream stream, PhotonMessageInfo info) {
+
+
+	}
+}
--- Assets/Scripts/StartUp.cs
+++ Assets/Scripts/StartUp.cs
@@ -10,7 +10,6 @@ public class StartUp : MonoBehaviour {
 	void Start () {
 		Instantiate (playerPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
 		Instantiate (targetPrefab, new Vector3 (0, 2, 0), Quaternion.identity);
-
 	}
 	
 	// Update is called once per frame
