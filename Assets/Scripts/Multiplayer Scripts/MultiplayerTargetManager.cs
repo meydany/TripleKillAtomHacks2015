11052094028da0cbc6fede1257d4d00d64b8873f
Yoli Meydan <meydany@bxscience.edu>
2015-05-30 19:36:41 -0400

    Target destroying

--- Assets/Scripts/Multiplayer Scripts/MultiplayerTargetManager.cs
+++ Assets/Scripts/Multiplayer Scripts/MultiplayerTargetManager.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class MultiplayerTargetManager : MonoBehaviour {
 	
 	public GameObject targetPrefab;
-	
+	public GameObject[] targets;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,12 +13,17 @@ public class MultiplayerTargetManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.FindGameObjectWithTag("Target") == null && PhotonNetwork.inRoom && PhotonNetwork.isMasterClient) {
+		if (GameObject.FindGameObjectWithTag("Target") == null && PhotonNetwork.isMasterClient) {
 			spawnNewTarget();
 		}
 	}
 	
 	void spawnNewTarget(){
+		targets = GameObject.FindGameObjectsWithTag ("Target");
+
+		foreach(GameObject target in targets) {
+			Destroy (target);
+		}
 		Vector3 newPos = new Vector3 (Random.Range(-(Camera.main.orthographicSize * Camera.main.aspect - (targetPrefab.GetComponent<Renderer>().bounds.size.x/2)), Camera.main.orthographicSize * Camera.main.aspect - (targetPrefab.GetComponent<Renderer>().bounds.size.x/2)), 
 		                              Random.Range (-(Camera.main.orthographicSize - targetPrefab.GetComponent<Renderer>().bounds.size.y/2), Camera.main.orthographicSize - targetPrefab.GetComponent<Renderer>().bounds.size.y/2), 0);
 		PhotonNetwork.Instantiate("MultiplayerTarget", newPos, Quaternion.identity,0,null);
