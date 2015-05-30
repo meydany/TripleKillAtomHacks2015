8f358630f2698c9ff40392085e923cbcb1ddaa8c
Oran Luzon <oran.luzon@gmail.com>
2015-05-30 13:14:06 -0400

    added timer and score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	
+	public int score;
+	
+	public GameObject ScoreText;
+	
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		ScoreText = GameObject.FindGameObjectWithTag ("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreText.GetComponent<Text> ().text = score.ToString ();        
+	}
+	
+	public void AddScore (int PointsAdded){
+		score += PointsAdded;
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	
+	public float TimeLeft;
+	
+	GameObject TimerText;
+	
+	// Use this for initialization
+	void Start () {
+		TimeLeft = 60f;
+		TimeLeft += 1f;
+		
+		TimerText = GameObject.FindGameObjectWithTag ("TimerText");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TimeLeft > 0) {
+			TimeLeft -= Time.deltaTime;
+		} 
+		else if (TimeLeft <= 0){
+			TimeLeft = 0;
+			// Make sure it only runs once ie. use boolean
+			//gameObject.GetComponent<EndGame>().GameOver();
+		}
+		TimerText.GetComponent<Text>().text = ((int)TimeLeft).ToString();            
+	}
+	
+	// Used to add time from Target script
+	public void AddTime(float TimeAdded){
+		
+		if (TimeLeft == 0f) {
+			TimeAdded = 0f;        
+		}
+		TimeLeft += TimeAdded;
+	}
+	
+	public void RemoveTime(float TimeRemoved){
+		
+		if (TimeLeft == 0f) {
+			TimeRemoved = 0f;
+		}
+		
+		TimeLeft -= TimeRemoved;
+	}
+}
