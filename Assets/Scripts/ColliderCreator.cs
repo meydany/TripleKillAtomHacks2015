f0a7aa858f15e833029996558600a4e34dcd6e3f
Oran Luzon <oran.luzon@gmail.com>
2015-05-30 09:40:38 -0400

    added the things

---
+++ Assets/Scripts/ColliderCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderCreator : MonoBehaviour {
+
+
+	GameObject leftWall, rightWall, topWall, bottomWall;
+
+	// Use this for initialization
+	void Start () {
+		rightWall = new Rect (Camera.main.orthographicSize * Camera.main.aspect, Camera.main.orthographicSize, 1, Camera.main.orthographicSize) as GameObject;
+		//rightWall.name = "rightWall";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
