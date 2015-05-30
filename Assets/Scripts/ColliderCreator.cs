da79c5fac39b302c37ed7789c3aec18bf6213d77
Oran Luzon <oran.luzon@gmail.com>
2015-05-30 10:26:50 -0400

    added stuff

--- Assets/Scripts/ColliderCreator.cs
+++ Assets/Scripts/ColliderCreator.cs
@@ -4,16 +4,38 @@ using System.Collections;
 public class ColliderCreator : MonoBehaviour {
 
 
-	GameObject leftWall, rightWall, topWall, bottomWall;
+	public GameObject rightWallPrefab, leftWallPrefab, topWallPrefab, botWallPrefab;
+
+	GameObject rightWall;
+	GameObject leftWall;
+	GameObject botWall;
+	GameObject topWall;
+
+	void Awake(){
+		rightWall = (GameObject)Instantiate (rightWallPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+		leftWall = (GameObject)Instantiate (leftWallPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+
+		topWall = (GameObject)Instantiate (topWallPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+		botWall = (GameObject)Instantiate (botWallPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+	}
 
 	// Use this for initialization
 	void Start () {
-		//rightWall = new Rect (Camera.main.orthographicSize * Camera.main.aspect, Camera.main.orthographicSize, 1, Camera.main.orthographicSize) as GameObject;
-		//rightWall.name = "rightWall";
+		createWalls ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void createWalls(){
+		float sideLimits = Camera.main.orthographicSize * Camera.main.aspect + (rightWall.GetComponent<Renderer> ().bounds.size.x / 2);
+		rightWall.transform.position = new Vector3 (sideLimits, 0, 0);
+		rightWall.transform.localScale = new Vector3(5, 0, 0);
+
+		leftWall.transform.position = new Vector3 (-sideLimits, 0, 0);
+
+		//float topLimit = Camera.main.orthographicSize + 
 	}
 }
